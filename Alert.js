87d4a2e24001a4bcbb8714313d6ba4b17c6a2441
zahrara22 <zahraraaputri@gmail.com>
2025-08-22 18:41:33 +0700

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,1 @@
+alert("Ini biodata Saya");
---
+++ Div id.js
@@ -0,0 +1,5 @@
+//membuat objek elemen
+      var nama = document . getElementById("Tujuan");
+      
+      // menampilkan output ke elemen hasil
+      nama.innerHTML = "<p>keinginan :haiking <br>Hobi : renang <br>Tujuan Hidup :sukses menjadi wanita karir </p>";
